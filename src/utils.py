e113a2bbc0c587fe07831fe5ff0c96a328803b65
Nikolay Vedernikov <nick.vedernikow@yandex.ru>
2022-05-05 16:13:33 +0300

    Add logging weights

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,26 @@
 import os
+import time
+from glob import glob
+from contextlib import contextmanager
+
+import wandb
+import torch
+import torchaudio
 
 from dotenv import load_dotenv
+
+
+class AugmentationNoise(torch.nn.Module):
+    def __init__(self, path_to_aug_sound, alpha=0.1):
+        super(AugmentationNoise, self).__init__()
+
+        audio, sr = torchaudio.load(path_to_aug_sound)
+        self.audio =  torch.unsqueeze(torch.mean(torchaudio.functional.resample(audio, sr, 16000), dim=0), dim=0)
+        self._noise_audio = audio.sum(dim=0)
+
+    def forward(self, wav, alpha):
+        # return wav + alpha * self._noise_audio[:wav.shape[-1]]
+        return wav + alpha * self.audio[:wav.shape[-1]]
 
 
 def get_wandb_token():
@@ -9,3 +29,17 @@ def get_wandb_token():
         return os.environ["WANDB_TOKEN"]
     except KeyError:
         raise RuntimeError("Eviron does not have wandb token!")
+
+
+@contextmanager
+def logging_model_weights(path):
+    def save_artifact(path_to_new_file):
+        artifact = wandb.Artifact(name="model weights", type="model weights")
+        artifact.add_file(path_to_new_file)
+        wandb.run.log_artifact(artifact)
+        wandb.run.join()
+    prev_weights = set(glob(path))
+    yield
+    current_weights = set(glob(path))
+    new_file, *_ = current_weights.difference(prev_weights)
+    save_artifact(*new_file)
